3b6d243e1bc9ba1a2ba0ec784c1242677e0f3db7
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-09-01 15:40:08 +0530

    add func to add two numbers

---
+++ tests/addition-test.js
@@ -0,0 +1,9 @@
+const { describe, it } = require("node:test");
+const assert = require("assert");
+const { add } = require("../src/addition");
+
+describe("addition", () => {
+  it("should add two numbers", () => {
+    assert.strictEqual(add(1, 2), 3);
+  });
+});
